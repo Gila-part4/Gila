f2a1c42887b5b55b377559af305097950d3b8ed8
sj0724 <sj07245@naver.com>
2024-09-20 10:54:08 +0900

    feat: 질문 상세 스켈레톤 구현

--- app/(protected)/(detail)/question/[questionId]/_components/question-detail.tsx
+++ app/(protected)/(detail)/question/[questionId]/_components/question-detail.tsx
@@ -1,38 +1,36 @@
+import { getQuestionById } from '@/app/data/question';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Separator } from '@/components/ui/separator';
-import { QuestionWithUserAndAnswers } from '@/type';
 import calculateDate from '@/utils/calculateData';
 
-export default function QuestionDetail({
-  questionInfo,
-}: {
-  questionInfo: QuestionWithUserAndAnswers;
-}) {
-  const createaAt = calculateDate(questionInfo.createdAt);
+export default async function QuestionDetail({ questionId }: { questionId: string }) {
+  const questionDetail = await getQuestionById({ questionId, answerTake: 10 });
+  if (!questionDetail) return <div>없음</div>;
+  const createaAt = calculateDate(questionDetail.createdAt);
 
   return (
     <div className="flex flex-col h-fit gap-3">
       <p className="text-left text-3xl font-bold leading-tight break-word pb-2">
-        {questionInfo.title}
+        {questionDetail.title}
       </p>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
-          <p className="text-sm font-semibold">{questionInfo.location}</p>
+          <p className="text-sm font-semibold">{questionDetail.location}</p>
           <p className="text-[12px] text-nowrap w-10 text-center text-gray_500">{`${createaAt.time}${createaAt.result}전`}</p>
         </div>
         <div className="flex justify-center items-center gap-2">
           <Avatar className="w-7 h-7">
             <AvatarImage
-              src={questionInfo.user.image || '/default-profile-image.png'}
+              src={questionDetail.user.image || '/default-profile-image.png'}
               className="object-cover w-7 h-7 rounded-full"
             />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <p className="text-sm">{questionInfo.user.nickname}</p>
+          <p className="text-sm">{questionDetail.user.nickname}</p>
         </div>
       </div>
       <Separator className="bg-gray_300 my-2" />
-      <div className="text-left break-words min-h-10">{questionInfo.content}</div>
+      <div className="text-left break-words min-h-10">{questionDetail.content}</div>
     </div>
   );
 }
--- app/(protected)/(detail)/question/[questionId]/page.tsx
+++ app/(protected)/(detail)/question/[questionId]/page.tsx
@@ -2,9 +2,11 @@
 import { getQuestionById } from '@/app/data/question';
 import { getCurrentUser } from '@/app/data/user';
 import { Separator } from '@/components/ui/separator';
+import { Suspense } from 'react';
 import QuestionDetail from './_components/question-detail';
 import AnswerList from './_components/answer-list';
 import AnswerForm from './_components/answer-form';
+import QuestionDetailSkeleton from './_components/question-detail-skeleton';
 
 interface Params {
   questionId: string;
@@ -16,7 +18,9 @@ export default async function Page({ params }: { params: Params }) {
   if (!questionDetail) return <div>없음</div>;
   return (
     <div className="p-4 flex flex-col gap-4">
-      <QuestionDetail questionInfo={questionDetail} />
+      <Suspense fallback={<QuestionDetailSkeleton />}>
+        <QuestionDetail questionId={params.questionId} />
+      </Suspense>
       <AnswerList
         answers={questionDetail.answers}
         totalCount={questionDetail._count.answers}
