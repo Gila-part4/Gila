4045ceaac265d47d85130065551b6f68b5fb46b1
sj0724 <sj07245@naver.com>
2024-09-24 13:45:51 +0900

    fix: 프로필, 대시보드 미들웨어 적용

--- middleware.ts
+++ middleware.ts
@@ -6,11 +6,12 @@ import { auth } from './auth';
 export default auth(async (req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-
   const isApiAuthRoute = nextUrl.pathname.startsWith(`/${apiAuthPrefix}`);
   const isApiUploadthingRoute = nextUrl.pathname.startsWith(`/${apiUploadThingPrefix}`);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
   const isMainPage = nextUrl.pathname === '/';
+  const isDashboardRoute = nextUrl.pathname.startsWith(`/dashboard`);
+  const isProfileRoute = nextUrl.pathname.startsWith(`/profile`);
 
   const isFirst = req.cookies.has('isFirstLogin');
   if (nextUrl.pathname !== '/topic') {
@@ -24,6 +25,10 @@ export default auth(async (req) => {
   }
   if (isApiUploadthingRoute) {
     return;
+  }
+
+  if ((!isLoggedIn && isDashboardRoute) || (!isLoggedIn && isProfileRoute)) {
+    return NextResponse.redirect(new URL('/sign-in', nextUrl));
   }
 
   if (isAuthRoute) {
