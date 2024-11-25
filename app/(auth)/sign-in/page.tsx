cd5be0f19f6de472df23687597729d2a433f2109
sj0724 <sj07245@naver.com>
2024-11-25 21:11:33 +0900

    fix: oauth버튼 적용

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,24 +1,12 @@
-'use client';
-
 import Link from 'next/link';
 import LoginForm from '@/app/(auth)/sign-in/_components/login-form';
-import { loginByOAuth } from '@/app/action/user';
-import { Button } from '@/components/ui/button';
-import KakaoIcon from '@/public/kakao-icon-black.svg';
-import Image from 'next/image';
+import OAuthButton from '../_components/oauth-button';
 
 export default function Page() {
   return (
     <div className="flex flex-col justify-center items-center gap-3 px-5">
       <LoginForm />
-      <p className="text-gray-500">or</p>
-      <Button
-        className="bg-white_light border w-full"
-        type="button"
-        onClick={() => loginByOAuth('kakao')}
-      >
-        <Image src={KakaoIcon} alt="카카오 아이콘" width={25} height={25} />
-      </Button>
+      <OAuthButton />
       <div className="flex items-center justify-center gap-x-2">
         <p className="text-gray-500">
           길라가 처음인가요?
